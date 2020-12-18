18e37c9ff24eba79bf2941f24ea38134348655b6
Justin Morrow <justin.parker.morrow@gmail.com>
2020-12-18 10:08:04 -0600

    first commit

---
+++ ThisApplication.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jmorrow
+ * Date: 9/17/2018
+ * Time: 6:28 AM
+ *
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using JPMorrow.Tools.Revit;
+using JPMorrow.Custom.Revit.Documents;
+
+namespace MainApp
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("58F7B2B7-BF6D-4B39-BBF8-13F7D9AAE97E")]
+	public partial class ThisApplication : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData cData, ref string message, ElementSet elements)
+        {
+			//set revit documents
+			DocPack.SetDocuments(cData);
+
+			//spool up search system
+			MepSystemSearchCustom mssc = new MepSystemSearchCustom();
+
+			List<ElementId> jboxes = DocPack.UIDOC.Selection.GetElementIds().ToList();
+
+			//fail if no boxes selected
+			if(!jboxes.Any())
+			{
+				RevitCustom.RevitCustomDebugger.Show(
+					header:	"Junction Boxes",
+					sub:	"None Selected",
+					err:	"No junction boxes were selected. " +
+							"please select them before running the script.");
+				return Result.Succeeded;
+			}
+
+			List<ElementId> boxesFiltered = new List<ElementId>();
+			foreach(ElementId id in jboxes)
+			{
+				Element box = DocPack.DOC.GetElement(id);
+				if (box.Category.Name != "Electrical Fixtures") continue;
+				if (String.IsNullOrWhiteSpace(box.LookupParameter("From").AsString()) ||
+					String.IsNullOrWhiteSpace(box.LookupParameter("To").AsString())) continue;
+				boxesFiltered.Add(id);
+			}
+
+			//check for parameters and quit if null
+			Element testBox = DocPack.DOC.GetElement(boxesFiltered.First());
+			if (testBox.LookupParameter("From") == null &&
+				testBox.LookupParameter("To") == null)
+			{
+				RevitCustom.RevitCustomDebugger.Show(
+					header: "Junction Boxes",
+					sub: "Parameters",
+					err: "You do not have the 'To' or 'From' parameters " +
+							"loaded for electrical fixtures. These parameters " +
+							"are required in order for this program to run.");
+				return Result.Succeeded;
+			}
+
+			int successCnt = 0;
+			using (TransactionGroup tgx = new TransactionGroup(DocPack.DOC, "Swapping Parameters"))
+			{
+				tgx.Start();
+				foreach(ElementId id in boxesFiltered)
+				{
+					using (Transaction tx = new Transaction(DocPack.DOC, "swapping parameters"))
+					{
+						tx.Start();
+						Element box = DocPack.DOC.GetElement(id);
+						string From = box.LookupParameter("From").AsString();
+						string To = box.LookupParameter("To").AsString();
+						box.LookupParameter("From").Set(To);
+						box.LookupParameter("To").Set(From);
+						tx.Commit();
+					}
+					successCnt++;
+
+				}
+				tgx.Assimilate();
+			}
+
+			RevitCustom.RevitCustomDebugger.Show(	header:"JBox To/From Swap",
+													sub:"Results",
+													err:successCnt.ToString() + " junction boxes had their parameters swapped.");
+			return Result.Succeeded;
+        }
+
+		#region startup
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+		#endregion
+
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+		#endregion
+	}
+}
---
+++ libs/RevitUserSelection.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelFactory
+{
+	/// <summary>
+	/// Revit User Selection of conduit.
+	/// </summary>
+	public class RSelect
+	{
+		/// <summary>Filter for selecting only conduit elements</summary>
+		public static ConduitSelectionFilter csf = new ConduitSelectionFilter();
+
+		/// <summary>List of user selected elements</summary>
+		private List<Element> _pickedElements = new List<Element>();
+
+		/// <summary>is the user selected list of elements empty</summary>
+		public bool IsEmpty { get { return _pickedElements.Count == 0 ? true : false; } }
+
+		/// <summary>
+		/// Promts the user to select 
+		/// elements from the active document
+		/// </summary>
+		/// <param name="doc">Revit Document</param>
+		/// <param name="uidoc">Revit UIDocument</param>
+		/// <param name="type">type of elements to select</param>
+		public RSelect(Document doc, UIDocument uidoc, SelectionType type)
+		{
+			switch(type)
+			{
+				case SelectionType.Conduit:
+					List<Element> els = ProcSelectedIds(doc, uidoc, SelectionType.Conduit);
+					if(els.Any())
+					{
+						_pickedElements = els.Any() ? els : new List<Element>();
+					}
+					break;
+			}
+		}
+		
+		/// <summary>
+		/// Proccess the references handed back by the user selection 
+		/// </summary>
+		/// <param name="doc">Revit Document</param>
+		/// <param name="refs">references to proccess</param>
+		/// <returns></returns>
+		private List<Element> ProcReferences(Document doc, List<Reference> refs)
+		{
+			List<ElementId> ids = refs.Select(x => x.ElementId).ToList();
+			List<Element> retList = new List<Element>();
+			foreach(ElementId id in ids)
+			{
+				retList.Add(doc.GetElement(id));
+			}
+			return retList;
+		}
+
+		private List<Element> ProcSelectedIds (Document doc, UIDocument uidoc, SelectionType type)
+		{
+			List<ElementId> ids = uidoc.Selection.GetElementIds().ToList();
+			List<Element> retList = new List<Element>();
+			foreach (ElementId id in ids)
+			{
+				Element el = doc.GetElement(id);
+				if (el.Category.Name == null) continue;
+				if(el.Category.Name == "Conduits")
+					retList.Add(el);
+			}
+			return retList;
+		}
+
+		/// <summary>
+		/// Get the Elements that the user selected
+		/// </summary>
+		/// <returns></returns>
+		public List<Element> GetUserPickedElements()
+		{
+			return _pickedElements;
+		}
+
+		public List<ElementId> GetUserPickedIds()
+		{
+			return _pickedElements.Select(x => x.Id).ToList();
+		}
+	}
+
+	public enum SelectionType
+	{
+		Conduit = 0,
+	}
+
+	public class ConduitSelectionFilter : ISelectionFilter
+	{
+		public bool AllowElement(Element elem)
+		{
+			if (elem.Category.Name != "Conduit")
+				return false;
+			return true;
+		}
+
+		public bool AllowReference(Reference reference, XYZ position)
+		{
+			return false;
+		}
+	}
+}
